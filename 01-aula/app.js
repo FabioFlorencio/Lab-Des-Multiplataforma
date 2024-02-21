6aa5b719900a227cdccccc705f90c47790cff016
Fabio Florencio <fabioflorencioxyz@gmail.com>
2024-02-21 10:47:46 -0300

    Primeira aula

---
+++ 01-aula/app.js
@@ -0,0 +1,27 @@
+/* var n1 = 10;
+
+var n2 = 2;
+
+var adicao = n1 + n2;
+var sub = n1 - n2;
+var mul = n1 * n2;
+var div = n1 / n2;
+
+console.log(adicao);
+console.log(sub);
+console.log(mul);
+console.log(div); */
+
+var soma = require('./soma.js');
+var sub = require('./sub.js');
+var mult = require('./mult.js');
+var div = require('./div.js');
+
+console.log(soma(10,2));
+console.log(sub(10,2));
+console.log(mult(10,2));
+console.log(div(10,2));
+
+
+
+
